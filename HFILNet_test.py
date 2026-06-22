01e6e672c3b2eef730805758fbb5d1ed2f930324
Luca Nicolas Terfehr <luca.terfehr@outlook.de>
2026-06-22 13:59:11 +0200

    Made script better in terms of argument passing

--- HFILNet_test.py
+++ HFILNet_test.py
@@ -9,6 +9,7 @@
 import torch
 import torch.nn.functional as F
 import sys
+import shutil
 
 sys.path.append('./models')
 import numpy as np
@@ -20,10 +21,12 @@ from data import test_dataset
 parser = argparse.ArgumentParser()
 parser.add_argument('--testsize', type=int, default=384, help='testing size')
 parser.add_argument('--gpu_id', type=str, default='0', help='select gpu id')
-parser.add_argument('--test_path', type=str, default='', help='test dataset path')
+# NEU: Die drei Quellpfade als separate, verpflichtende Argumente
+parser.add_argument('--rgb_path', type=str, required=True, help='path to RGB images')
+parser.add_argument('--gt_path', type=str, required=True, help='path to Ground Truth (GT) images')
+parser.add_argument('--depth_path', type=str, required=True, help='path to depth maps')
+parser.add_argument('--target', type=str, required=True, help='the directory to save the test results')
 opt = parser.parse_args()
-
-dataset_path = opt.test_path
 
 # set device for test
 if opt.gpu_id == '0':
@@ -35,42 +38,50 @@ elif opt.gpu_id == '1':
 
 # load the model
 model = HFILNet()
-model.load_state_dict(torch.load(''))
+model.load_state_dict(torch.load('caps/HFILNet_epoch_best.pth'))
 model.cuda()
 model.eval()
 
+# Zielordner vorbereiten (Idempotent)
+save_path = opt.target + "/"
+if os.path.exists(save_path):
+    print(f"{save_path} already exists, deleting it")
+    shutil.rmtree(save_path)
+os.makedirs(save_path, exist_ok=False)
 
-test_datasets = []
-for dataset in test_datasets:
-    save_path = '' + dataset + '/'
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-    image_root = dataset_path + dataset + '/RGB/'
-    gt_root = dataset_path + dataset + '/GT/'
-    depth_root = dataset_path + dataset + '/depth/'
-    test_loader = test_dataset(image_root, gt_root, depth_root, opt.testsize)
-    for i in range(test_loader.size):
-        image, gt, depth, name, image_for_post = test_loader.load_data()
-        gt = np.asarray(gt, np.float32)
-        gt /= (gt.max() + 1e-8)
-        image = image.cuda()
-        depth = depth = depth.repeat(1, 3, 1, 1).cuda()
-        out1, out2, out3 = model(image, depth)
+# Slashes am Ende garantieren, falls sie beim Aufruf vergessen wurden
+image_root = opt.rgb_path if opt.rgb_path.endswith('/') else opt.rgb_path + '/'
+gt_root = opt.gt_path if opt.gt_path.endswith('/') else opt.gt_path + '/'
+depth_root = opt.depth_path if opt.depth_path.endswith('/') else opt.depth_path + '/'
 
-        out1 = F.upsample(out1, size=gt.shape, mode='bilinear', align_corners=False)
-        out2 = F.upsample(out2, size=gt.shape, mode='bilinear', align_corners=False)
-        out3 = F.upsample(out3, size=gt.shape, mode='bilinear', align_corners=False)
+# Test-Loader direkt mit den übergebenen Argumenten füttern
+test_loader = test_dataset(image_root, gt_root, depth_root, opt.testsize)
 
-        out1 = out1.sigmoid().data.cpu().numpy().squeeze()
-        out2 = out2.sigmoid().data.cpu().numpy().squeeze()
-        out3 = out3.sigmoid().data.cpu().numpy().squeeze()
+for i in range(test_loader.size):
+    image, gt, depth, name, image_for_post = test_loader.load_data()
+    gt = np.asarray(gt, np.float32)
+    gt /= (gt.max() + 1e-8)
+    image = image.cuda()
+    
+    depth = depth.repeat(1, 3, 1, 1).cuda()
+    out1, out2, out3 = model(image, depth)
 
-        out1 = (out1 - out1.min()) / (out1.max() - out1.min() + 1e-8)
-        out2 = (out2 - out2.min()) / (out2.max() - out2.min() + 1e-8)
-        out3 = (out3 - out3.min()) / (out3.max() - out3.min() + 1e-8)
-        print('save img to: ', save_path + name)
+    out1 = F.upsample(out1, size=gt.shape, mode='bilinear', align_corners=False)
+    out2 = F.upsample(out2, size=gt.shape, mode='bilinear', align_corners=False)
+    out3 = F.upsample(out3, size=gt.shape, mode='bilinear', align_corners=False)
 
-        cv2.imwrite(save_path + name, out3 * 255)
-    print('Test Done!')
+    out1 = out1.sigmoid().data.cpu().numpy().squeeze()
+    out2 = out2.sigmoid().data.cpu().numpy().squeeze()
+    out3 = out3.sigmoid().data.cpu().numpy().squeeze()
+
+    out1 = (out1 - out1.min()) / (out1.max() - out1.min() + 1e-8)
+    out2 = (out2 - out2.min()) / (out2.max() - out2.min() + 1e-8)
+    out3 = (out3 - out3.min()) / (out3.max() - out3.min() + 1e-8)
+    print('save img to: ', save_path + name)
+
+    os.makedirs(os.path.dirname(save_path + name), exist_ok=True)
+    cv2.imwrite(save_path + name, out3 * 255)
+
+print('Test Done!')
 
 # #######################################################  end  ######################################
